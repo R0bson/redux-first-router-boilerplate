d332ab967548e3669c9c9b56b86648c77476a759
Robert S <sabiszrobert@gmail.com>
2017-10-01 01:18:58 +0200

    Added source of requireAll stories function

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,10 @@
 import { configure } from '@storybook/react'
 
+
+/*
+  Requires all *.story.js files from src files and stories/index.js file.
+  Based on: https://github.com/storybooks/storybook/issues/125#issuecomment-212386581
+ */
 function requireAll(requireContext) {
   return requireContext.keys().map(requireContext)
 }
